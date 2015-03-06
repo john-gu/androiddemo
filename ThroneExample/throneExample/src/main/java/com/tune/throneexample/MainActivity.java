1ebada05221fbb7ab7e86af2540a849e1f5690ee
john-gu <johng@tune.com>
2015-03-06 13:03:05 -0800

    Working App Alliances implementation

--- ThroneExample/throneExample/src/main/java/com/tune/throneexample/MainActivity.java
+++ ThroneExample/throneExample/src/main/java/com/tune/throneexample/MainActivity.java
@@ -1,14 +1,23 @@
 package com.tune.throneexample;
 
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 
 import com.example.throneexample.R;
+import com.tune.alliances.TuneAdOrientation;
 import com.tune.alliances.TuneBanner;
+import com.tune.alliances.TuneBannerPosition;
 import com.tune.alliances.TuneInterstitial;
 import com.tune.mat.MobileAppTracker;
+
+import static com.google.android.gms.ads.identifier.AdvertisingIdClient.Info;
+import static com.google.android.gms.ads.identifier.AdvertisingIdClient.getAdvertisingIdInfo;
 
 public class MainActivity
     extends ActionBarActivity {
@@ -25,46 +34,47 @@ public class MainActivity
         setContentView(R.layout.activity_main);
         
         // Init MAT
-/*        MobileAppTracker.init(getApplicationContext(), getString(R.string.mat_advertiser_id), getString(R.string.mat_conversion_key));
+        MobileAppTracker.init(getApplicationContext(), getString(R.string.mat_advertiser_id), getString(R.string.mat_conversion_key));
         mat = MobileAppTracker.getInstance();
-        
+        mat.setPackageName("com.test");
+
         // Collect Google Play Advertising ID
         new Thread(new Runnable() {
             @Override
             public void run() {
                 // See sample code at http://developer.android.com/google/play-services/id.html
                 try {
-                    Info adInfo = AdvertisingIdClient.getAdvertisingIdInfo(getApplicationContext());
+                    Info adInfo = getAdvertisingIdInfo(getApplicationContext());
                     mat.setGoogleAdvertisingId(adInfo.getId(), adInfo.isLimitAdTrackingEnabled());
                 } catch (Exception e) {
-                    mat.setAndroidId(Secure.getString(getContentResolver(), Secure.ANDROID_ID));
+                    mat.setAndroidId(Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID));
                 }
             }
-        }).start();*/
+        }).start();
         
         myView = (MyView) findViewById(R.id.my_view);
         myView.setBackgroundResource(R.drawable.sky);
         
-        /*Button clearButton = (Button) findViewById(R.id.clear_button);
-        clearButton.setOnClickListener(new OnClickListener() {
+        Button clearButton = (Button) findViewById(R.id.clear_button);
+        clearButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                myView.clearCanvas();
+                //myView.clearCanvas();
                 if (tuneInterstitial.hasLoaded()) {
                     tuneInterstitial.show();
                     tuneInterstitial.load();
                 }
             }
-        });*/
+        });
         
-/*        tuneBanner = new TuneBanner(this);
+        tuneBanner = new TuneBanner(this);
         RelativeLayout mainLayout = (RelativeLayout) findViewById(R.id.main_layout);
         mainLayout.addView(tuneBanner);
         tuneBanner.setPosition(TuneBannerPosition.BOTTOM_CENTER);
         tuneBanner.load();
-*/        
-/*        tuneInterstitial = new TuneInterstitial(this);
-        tuneInterstitial.load();*/
+
+        tuneInterstitial = new TuneInterstitial(this, TuneAdOrientation.PORTRAIT_ONLY);
+        tuneInterstitial.load();
     }
     
     @Override
@@ -73,6 +83,15 @@ public class MainActivity
         
 //        mat.setReferralSources(this);
 //        mat.measureSession();
+
+        tuneBanner.resume();
+    }
+
+    @Override
+    protected void onPause() {
+        tuneBanner.pause();
+
+        super.onPause();
     }
  
     @Override
